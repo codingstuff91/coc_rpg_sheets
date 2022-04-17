9881a462539f35c15f093acd016647b46af51d7a
Matthieu MARTIN <37110847+codingstuff91@users.noreply.github.com>
2022-04-17 23:00:04 +0200

    Character creation (#8)
    * Correction Models
    
    * Correction route user
    
    * Register a new user
    
    * Mongoose db connection refactor
    
    * User register refactor
    
    * User login
    
    * Character model initialisation
    
    * Edit character characteristics
    
    * Add description schema
    
    * Add weapons caracteristics
    
    * Add equipment schema
    
    * Create a new character

--- backend/models/Character.js
+++ backend/models/Character.js
@@ -1,7 +1,165 @@
 const mongoose = require("mongoose");
 
+const characteristicSchema = new mongoose.Schema({
+    strength: {
+        score: {
+            type: Number,
+            min: 0,
+            max: 20,
+            default: 0,
+        },
+        mod: {
+            type: String,
+        },
+    },
+    dexterity: {
+        score: {
+            type: Number,
+            min: 0,
+            max: 20,
+            default: 0,
+        },
+        mod: {
+            type: String,
+        },
+    },
+    constitution: {
+        score: {
+            type: Number,
+            min: 0,
+            max: 20,
+            default: 0,
+        },
+        mod: {
+            type: String,
+        },
+    },
+    intelligence: {
+        score: {
+            type: Number,
+            min: 0,
+            max: 20,
+            default: 0,
+        },
+        mod: {
+            type: String,
+        },
+    },
+    perception: {
+        score: {
+            type: Number,
+            min: 0,
+            max: 20,
+            default: 0,
+        },
+        mod: {
+            type: String,
+        },
+    },
+    charisma: {
+        score: {
+            type: Number,
+            min: 0,
+            max: 20,
+            default: 0,
+        },
+        mod: {
+            type: String,
+        },
+    },
+});
+
+const vitalitySchema = new mongoose.Schema({
+    health_dice: Number,
+    health_points: Number,
+    health_points_max: Number,
+});
+
+const luckSchema = new mongoose.Schema({
+    luck_points: Number,
+    luck_points_max: Number,
+});
+
+const combatSchema = new mongoose.Schema({
+    initiative: Number,
+    contact_attack: Number,
+    ranged_attack: Number,
+    mental_attack: Number,
+});
+
+const weaponSchema = new mongoose.Schema({
+	name : {
+		type : String
+	},
+	ranged_attack : {
+		type : String
+	},
+	damage_score : {
+		type : String
+	},
+	special : {
+		type : String
+	}
+});
+
+const descriptionSchema = new mongoose.Schema({
+    gender: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    height: {
+        type: String,
+    },
+    languages: [String],
+});
+
+const equipmentSchema = new mongoose.Schema({
+	object : {
+		type : String
+	},
+	equiped : {
+		type : Boolean
+	}
+})
+
 const schema = new mongoose.Schema({
-  name: "string",
+    name: {
+        type: String,
+        required: true,
+    },
+    level: {
+        type: Number,
+        required: true,
+    },
+    profile: {
+        type: String,
+        required: true,
+    },
+    trait: {
+        type: String,
+        required: true,
+    },
+    living_level: {
+        type: String,
+        required: true,
+    },
+    description: [descriptionSchema],
+    characteristics: [characteristicSchema],
+    vitality: [vitalitySchema],
+    luck_points: [luckSchema],
+    combat: [combatSchema],
+	weapons : [weaponSchema],
+	equipment : [equipmentSchema],
+    defense: {
+        armor: {
+            type: Number,
+            default: 0,
+        },
+    },
 });
 
 const character = mongoose.model("Character", schema);
--- backend/routes/characterRouter.js
+++ backend/routes/characterRouter.js
@@ -1,10 +1,27 @@
 var express = require("express");
 var router = express.Router();
-var database = require('../config/database')
-const Character = require('../models/Character')
+const Character = require("../models/Character");
 
-router.get("/", function (req, res, next) {
-    
+/**
+ * Afficher tous les personnages
+ */
+router.get("/", async (req, res, next) => {
+    let characters = await Character.find({});
+    res.json(characters);
+});
+
+/**
+ * Créer un nouveau personnage
+ */
+router.post("/", async (req, res) => {
+    const new_character = new Character(req.body);
+
+    try {
+        let character = await new_character.save();
+        res.json({ message: "Nouveau personnage crée" });
+    } catch (e) {
+        res.json({ message: e.message });
+    }
 });
 
 module.exports = router;
