1cf38b167b502c567851671e2386e1b1465843ae
Matthieu MARTIN <codingstuff91@gmail.com>
2022-04-17 13:04:29 +0200

    User register refactor

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,29 +1,26 @@
 var express = require("express");
 var router = express.Router();
-const database = require("../config/database");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 /**
  * Inscription d'un nouvel utilisateur
  */
-router.post("/register", (req, res) => {
-	const salt = bcrypt.genSaltSync(10);
-	const password = bcrypt.hashSync(req.body.password, salt);
+router.post("/register", async (req, res) => {
+    const salt = bcrypt.genSaltSync(10);
+    const password = bcrypt.hashSync(req.body.password, salt);
 
-	const new_user = new User({
-		name : req.body.name,
-		email : req.body.email,
-		password : password
-	});
-
-    database().then(() => {
-        new_user.save().then(() => {
-            res.json({message : "Votre inscription a été prise en compte"});
-        }).catch((error)=>{
-			console.log(error);
-		});
+    const new_user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: password,
     });
+    try {
+        let new_user_created = await new_user.save();
+        res.json(new_user_created);
+    } catch (e) {
+        console.log(e.message);
+    }
 });
 
 module.exports = router;
