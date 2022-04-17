6e2da2abbd0ac41e6ae5dcb25b8e445420b9adbb
Matthieu MARTIN <37110847+codingstuff91@users.noreply.github.com>
2022-04-17 18:43:49 +0200

    User login and register refactor (#5)
    * Correction Models
    
    * Correction route user
    
    * Register a new user
    
    * Mongoose db connection refactor
    
    * User register refactor
    
    * User login

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,29 +1,58 @@
 var express = require("express");
 var router = express.Router();
-const database = require("../config/database");
-const bcrypt = require('bcryptjs');
+
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 /**
  * Inscription d'un nouvel utilisateur
  */
-router.post("/register", (req, res) => {
-	const salt = bcrypt.genSaltSync(10);
-	const password = bcrypt.hashSync(req.body.password, salt);
+router.post("/register", async (req, res) => {
+    const salt = bcrypt.genSaltSync(10);
+    const password = bcrypt.hashSync(req.body.password, salt);
 
-	const new_user = new User({
-		name : req.body.name,
-		email : req.body.email,
-		password : password
-	});
-
-    database().then(() => {
-        new_user.save().then(() => {
-            res.json({message : "Votre inscription a été prise en compte"});
-        }).catch((error)=>{
-			console.log(error);
-		});
+    const new_user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: password,
     });
+    try {
+        let new_user_created = await new_user.save();
+        res.json(new_user_created);
+    } catch (e) {
+        console.log(e.message);
+    }
+});
+
+/**
+ * Connexion d'un utilisateur
+ */
+router.post("/login", async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return res.status(400).json({
+            message: "Aucun utilisateur trouvé avec cette adresse mail",
+        });
+    }
+
+    const password_match = await bcrypt.compare(
+        req.body.password,
+        user.password
+    );
+
+    if (!password_match) {
+        return res
+            .status(400)
+            .json({ message: "Mauvais mot de passe ! Veuillez ré essayer" });
+    }
+
+    const auth_token = await jwt.sign({ user: user }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
+
+    res.json({user, auth_token});
 });
 
 module.exports = router;
