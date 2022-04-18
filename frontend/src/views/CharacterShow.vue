084d7991cf87f17761487abcbdba2f198d2eff08
Matthieu MARTIN <codingstuff91@gmail.com>
2022-04-18 17:42:13 +0200

    Initiate characterShow view component

---
+++ frontend/src/views/CharacterShow.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Nom : {{ character_details.name }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        character_details() {
+            return this.$store.state.character;
+        },
+    },
+    mounted() {
+        this.$store.dispatch("getCharacter", {
+            character_id: this.$route.params.character_id,
+        });
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
