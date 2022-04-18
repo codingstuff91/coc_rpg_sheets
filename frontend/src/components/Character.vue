773ee870bff09cf3bd43f8c2b3df89b0d6ebfbf5
Matthieu MARTIN <codingstuff91@gmail.com>
2022-04-18 14:05:10 +0200

    Create character component

---
+++ frontend/src/components/Character.vue
@@ -0,0 +1,34 @@
+<template>
+    <el-container>
+        <div class="character_card">
+            <h2>{{ character.name }}</h2>
+            <p>{{ character.profile }}</p>
+            <el-button type="info" round>Consulter</el-button>
+        </div>
+    </el-container>
+</template>
+
+<script>
+export default {
+    props: {
+        character: {
+            type: Array,
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+.character_card {
+    border: 2px solid red;
+    margin: 0 30px;
+    padding : 20px;
+    border-radius : 10px;
+}
+</style>
