02e223c33a649b28639aade29170ac0ef5d404df
Matthieu MARTIN <codingstuff91@gmail.com>
2022-04-18 14:05:19 +0200

    Create characters list component

---
+++ frontend/src/views/CharactersList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>PERSONNAGES DISPONIBLES</h1>
+        <div class="wrapper">
+            <Character v-for="(character, index) in caractersList" :key="index" :character="character"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Character from "../components/Character.vue";
+
+export default {
+    computed: {
+        caractersList() {
+            return this.$store.state.characters
+        },
+    },
+    components: {
+        Character,
+    },
+    async mounted() {
+        this.$store.dispatch("getCharactersList");
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+h1{
+    margin-bottom: 20px;
+}
+
+.wrapper{
+    display: flex;
+    justify-content: center;
+}
+</style>
