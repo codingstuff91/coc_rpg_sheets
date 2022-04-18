3cfad2441f3ed0170a178863815575c06b2cc638
Matthieu MARTIN <codingstuff91@gmail.com>
2022-04-18 14:04:53 +0200

    Character store initialisation

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,22 @@
 import { createStore } from "vuex";
+import CharacterService from "../services/CharacterService";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    characters : []
+  },
+  mutations: {
+    SET_CHARACTERS(state, characters){
+      state.characters = characters
+    }
+  },
+  actions: {
+    getCharactersList({ commit }){
+      CharacterService.getCharacters()
+      .then(({data})=>{
+        commit('SET_CHARACTERS', data)
+      })
+    }
+  },
   modules: {},
 });
